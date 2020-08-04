f8c8b88e5fbbf9344e3b34fc1169bf8007744ad2
kderbyma_ <kyle@kderbyma.com>
2020-08-03 18:09:30 -0600

    fixing ssl / http confusion on gRPC calls

--- portal-srv/etc/Env.conf.js
+++ portal-srv/etc/Env.conf.js
@@ -33,7 +33,7 @@ module.exports = {
     // XRP Lite Node (Docker Container)
     XRP_HOST: "xrpl_server",
     XRP_PORT: "5005",
-    XRP_PROTOCOL: "https://",
+    XRP_PROTOCOL: "http://",
     // Xpring SDK (Test Net)
     XPRING_HOST: "test.xrp.xpring.io",
     XPRING_PORT: "50051",
